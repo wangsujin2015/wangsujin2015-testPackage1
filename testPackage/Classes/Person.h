aa08150e46c9676770b4544e1c567064342de5da
Alpha <alpha@cg.app>
2021-07-02 10:41:05 +0800

    3

---
+++ testPackage/Classes/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  acustomerchat_Tests
+//
+//  Created by user on 02/07/2021.
+//  Copyright © 2021 alpha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ testPackage/Classes/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  acustomerchat_Tests
+//
+//  Created by user on 02/07/2021.
+//  Copyright © 2021 alpha. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
